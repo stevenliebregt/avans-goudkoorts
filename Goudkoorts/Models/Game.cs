a614537c46c4d7c29d1d56f67ffffbc2cff403a7
Steven Liebregt <s.liebregt@outlook.com>
2019-10-10 18:11:19 +0200

    added input

--- Goudkoorts/Models/Game.cs
+++ Goudkoorts/Models/Game.cs
@@ -13,6 +13,9 @@ namespace Goudkoorts.Models
 
         private readonly List<Action<Game>> _gameTickObservers = new List<Action<Game>>();
         
+        private AutoResetEvent _autoResetEvent;
+        private Timer _timer;
+        
         private bool _isOver = false;
 
         public int Score { get; private set; } = 0;
@@ -26,15 +29,30 @@ namespace Goudkoorts.Models
         {
             _gameTickObservers.Add(gameTickObserver);
         }
+
+        public void SwitchTrack(int trackId)
+        {
+            Console.WriteLine($"Switching track: {trackId}");
+            
+            // TODO: Implement this
+        }
         
         public void Run()
         {
-            var autoEvent = new AutoResetEvent(false);
-            var timer = new Timer(Tick, autoEvent, 0, _intervalMilliseconds);
+            _autoResetEvent = new AutoResetEvent(false);
+            _timer = new Timer(Tick, _autoResetEvent, 0, _intervalMilliseconds);
 
             // Start the loop, and throw away the timer once it is done.
-            autoEvent.WaitOne();
-            timer.Dispose();
+            _autoResetEvent.WaitOne();
+            _timer.Dispose();
+        }
+
+        public void Stop()
+        {
+            // TODO: Does AutoResetEvent.Dispose() need to be called?
+            
+            _autoResetEvent.Set();
+            _timer.Dispose();
         }
 
         private void Tick(object stateInfo)
--- Goudkoorts/Views/InputView.cs
+++ Goudkoorts/Views/InputView.cs
@@ -1,7 +1,31 @@
-﻿namespace Goudkoorts.Views
+﻿using System;
+
+namespace Goudkoorts.Views
 {
     public class InputView
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="minimumTrackId">The lowest track id we allow to be pressed</param>
+        /// <param name="maximumTrackId">The highest track id we allow to be pressed</param>
+        /// <param name="callback">A method that takes a bool and an int, the bool tells if the 'exit' key was
+        /// pressed, and the int is the id of the track to switch.</param>
+        public void OnSwitchTrackIdPressed(int minimumTrackId, int maximumTrackId, Action<bool, int> callback)
+        {
+            while (true)
+            {
+                var consoleKey = Console.ReadKey().Key;
+
+                if (consoleKey == ConsoleKey.Escape)
+                {
+                    callback(true, 0);
+                }
+                else
+                {
+                    callback(false, 1);
+                }
+            }
+        }
     }
 }
